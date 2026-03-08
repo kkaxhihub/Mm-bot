5ed7a5eb7dc1b33e5bcb7f71343311d1cbcb6b2a
kkaxhihub <sujathasp41@gmail.com>
2026-03-08 13:40:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -518,11 +518,12 @@ async def warn(
 
     embed.set_footer(text="Powered by Kakashi")
 
-   log_channel=interaction.guild.get_channel(LOG_CHANNEL_ID)
+   log_channel=
+interaction.guild.get_channel(LOG_CHANNEL_ID)
             if log_channel:
                 await log_channel.send(embed=embed)
 
-            await interaction.response.send_message(
+    await interaction.response.send_message(
                 f"✅ {user.mention} has been warned.",
                 ephemeral=True
             )
